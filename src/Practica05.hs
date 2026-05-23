2447b844d7a58a9ca85f77074d7e1510be00b394
AlfredGar <alfredo.contreras09@ciencias.unam.mx>
2026-05-23 07:26:06 -0600

    70%

--- src/Practica05.hs
+++ src/Practica05.hs
@@ -3,20 +3,30 @@ module Practica05 where
 import Terminos
 
 --Aplicar una sustitucion a un termino
-apsubT :: Term -> Subst -> Term
-apsubT = undefined 
 
---Funcion auxiliar para aplicar la sustitucion a una lista de terminos
+apsubT :: Term -> Subst -> Term
+apsubT (Var name) sub = case lookup name sub of
+    Just t  -> t
+    Nothing -> Var name
+apsubT (Fun name args) sub = Fun name (aplicarLista args sub)
+
+-- Función auxiliar para aplicar la sustitución a una lista de términos
 aplicarLista :: [Term] -> Subst -> [Term]
-aplicarLista = undefined 
+aplicarLista ts sub = map (\t -> apsubT t sub) ts
 
 --Funcion que elimina los pares que son de la forma x=x
 simpSus :: Subst -> Subst
-simpSus = undefined
-
+simpSus [] = []
+simpSus ((x,t):xs)
+  | t == Var x = simpSus xs
+  | otherwise = (x,t) : simpSus xs
+  
 --Funcion que calcula la composicion de dos sustituciones
 compSus :: Subst -> Subst -> Subst
-compSus = undefined
+compSus s1 s2 = simpSus (aplicados ++ restantes)
+  where
+    aplicados = [ (x, apsubT t s2) | (x, t) <- s1]
+    restantes = [ (y, t) | (y, t) <- s2, notElem y (map fst s1) ]
 
 --Funcion que devuelve un umg de dos terminos, si es que lo hay
 unifica :: Term -> Term -> [Subst]
